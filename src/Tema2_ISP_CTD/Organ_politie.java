e3c302003ea257eeea6279715ba86eff58e47cd9
Moldo <dan31081997@gmail.com>
2019-05-04 12:37:45 +0300

    Am adaugat functii la cls Organ_politie, mai trebuiesc facute modificari la cls Sanctiune

--- src/Tema2_ISP_CTD/Organ_politie.java
+++ src/Tema2_ISP_CTD/Organ_politie.java
@@ -4,6 +4,10 @@
 
 package Tema2_ISP_CTD;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 /************************************************************/
 /**
  * 
@@ -27,8 +31,46 @@ public class Organ_politie extends Date_Rovinieta {
 		return this.nume;
 	}
 	
-	public void aplicareSanctiune() 
-	{
+	/**
+	 *  Fac override la fct verificareRovinieta din cls Date_Rovinieta 
+	 */
+	
+	@Override 
+	
+	public boolean verificareRovinieta(long id) {
+		if(super.verificareRovinieta(id) == false) {
+			 this.aplicareSanctiune();
+			 return false;
+		}
+		return true;
+		
+		
+		
+	}
+	/*
+	 * 
+	 */
+	
+	public void aplicareSanctiune() {
+		
+		String proba = "Rovinieta expirata";
+		
+		String loc = "DN2";
+		
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Calendar cal = Calendar.getInstance();
+		
+		
+		String data = dateFormat.format(cal.getTime()).toString();
+		
+		
+		/**
+		 * Ba, am scos campul ora din sanctiune, ca oricum o sa fie in data
+		 * 
+		 * !Vezi  ca obiectul meu "sanctiune" sa l apeleze ok.
+		 * 
+		 */
+		return new Sanctiune(proba, data, loc, this);
 		
 	}
 };
